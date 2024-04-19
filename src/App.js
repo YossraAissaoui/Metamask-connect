a09ea87857e3401c102fcf7fbbed61d22c84a21f
Yosra Aissaoui <97959784+YossraAissaoui@users.noreply.github.com>
2024-04-19 14:45:51 +0100

    WalletCard

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg'; 
+
+import './App.css'; 
+
+import WalletCard from './WalletCard'; 
+
+
+  
+
+function App() { 
+
+  
+
+  return ( 
+
+    <div className="App"> 
+
+    <WalletCard/> 
+
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default App; 
